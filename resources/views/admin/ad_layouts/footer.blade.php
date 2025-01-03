cbc61e1d35eea67d15c1813c0d65979baee097df
ZobayerZ <zobayerzim10872@gmail.com>
2025-01-03 21:56:03 +0600

    Delete resources directory

--- resources/views/admin/ad_layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-
-<!-- Main Footer -->
-<footer class="main-footer">
-    <strong>Copyright &copy; {{ date('Y') }} Echo Merch.com</strong>
-  </footer>
